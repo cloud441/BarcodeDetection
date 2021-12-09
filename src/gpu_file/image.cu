bf3f9c2638ace23ca21505a4dd3fa46b2f1277b7
Leherle maxime <maxime.leherle@epita.fr>
2021-12-09 19:34:41 +0100

    test4 gray

--- src/gpu_file/image.cu
+++ src/gpu_file/image.cu
@@ -55,8 +55,11 @@ void Image::create_gray_array()
     // CPU Version
     for (int i = 0; i < height * width; ++i)
     {
+        /*
         img_gray_array[i] = img_array[i * 3] * 0.2989 +
             img_array[i * 3 + 1] * 0.5870 + img_array[i * 3 + 2] * 0.1140;
+        */
+        img_gray_array[i] = (uint8_t)((img_array[i * 3] + img_array[i * 3 + 1] + img_array[i * 3 + 2])/3.0);
     }
 
 }
@@ -69,11 +72,31 @@ int Image::get_size()
 
 int main(void)
 {
-    Image image("../../img/codebar.jpg");
+    //Image image("../../img/codebar.jpg");
 
-    image.create_gray_array();
+    //image.create_gray_array();
 
-    image.save_gray_img();
+    //image.save_gray_img();
+
+    int width, height, nb_chan;
+    unsigned char *img = stbi_load("../../img/codebar.jpg", &width, &height, &nb_chan, 0);
+
+    if (img == NULL)
+    {
+        printf("Error !\n");
+        exit(1);
+    }
+
+
+    size_t img_size = width * height;
+    unsigned char *gray_img = (unsigned char *) malloc(img_size);
+
+    for (unsigned char *p = img, *pg = gray_img; p != img + img_size; p += 3, p += 1)
+    {
+        *pg = (uint8_t)((*p + *(p + 1) + *(p + 2))/3.0);
+    }
+
+    stbi_write_jpg("../../img/img_gray_result.jpg", width, height, 1, gray_img, 100);
 
     return 0;
 }
